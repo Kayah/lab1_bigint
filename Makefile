100a88b6581be2cd406078c562b68c7b50c9c247
Aleksandr Konoriev <skiffertyp@gmail.com>
2018-03-25 13:31:15 +0300

    added google tests

--- Makefile
+++ Makefile
@@ -1,30 +1,59 @@
-CC=gcc
 CXX=g++
-SRCS= biginteger.c
-SRCXXS = main.cpp bigintarithmetic.cpp
-OBJS=$(SRCS:.c=.o)
-OBJS+=$(SRCXXS:.cpp=.o)
-MAIN=lab1
-# CFLAGS= -O3 -Werror -fcolor-diagnostics -fsanitize=memory -fsanitize=undefined
-# CFLAGS=
-LFLAGS=
-LIBS=
-
-ifeq ($(DEBUG), y)
-CFLAGS += -DDEBUG
+ifeq ($(USE_CCACHE), y)
+ifeq (, $(shell which ccache))
+$(error "ccache is not installed, please do apt-get install ccache")
+else 
+CXX=ccache g++
+endif
 endif
 
-build: $(MAIN)
+GTEST_DIR = googletest/googletest
 
-$(MAIN): $(OBJS)
-	$(CXX) $(CFLAGS) $(INCLUDES) -o $(MAIN) $(OBJS) $(LFLAGS) $(LIBS)
+CPPFLAGS += -isystem $(GTEST_DIR)/include
 
-.c.o:
-	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+CXXFLAGS += -g -Wall -Wextra -pthread 
 
-.cpp.o:
-	$(CXX) $(CFLAGS) $(INCLUDES) -c $< -o $@
+TESTS = main
 
-clean:
-	rm *.o $(MAIN)
+SOURCE_DIR = src
 
+SRCS= $(SOURCE_DIR)/bigintarithmetic.cpp \
+	  $(SOURCE_DIR)/biginteger.cpp\
+	  $(SOURCE_DIR)/main.cpp
+
+OBJS=$(SRCS:.cpp=.o)
+
+GTEST_HEADERS = $(GTEST_DIR)/include/gtest/*.h \
+                $(GTEST_DIR)/include/gtest/internal/*.h
+
+ifeq ($(DEBUG), y)
+CPPFLAGS += -DDEBUG
+endif
+
+all : $(TESTS)
+	./$(TESTS)
+
+clean :
+	rm -rf $(TESTS) gtest.a gtest_main.a src/*.o src/*.gch test/*.o
+
+GTEST_SRCS_ = $(GTEST_DIR)/src/*.cc $(GTEST_DIR)/src/*.h $(GTEST_HEADERS)
+
+gtest-all.o : $(GTEST_SRCS_)
+	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) $(CXXFLAGS) -c \
+            $(GTEST_DIR)/src/gtest-all.cc
+
+gtest_main.o : $(GTEST_SRCS_)
+	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR) $(CXXFLAGS) -c \
+            $(GTEST_DIR)/src/gtest_main.cc
+
+gtest.a : gtest-all.o
+	$(AR) $(ARFLAGS) $@ $^
+
+gtest_main.a : gtest-all.o gtest_main.o
+	$(AR) $(ARFLAGS) $@ $^
+
+.cpp.o: $(SOURCE_DIR)/bigintarithmetic.h $(SOURCE_DIR)/biginteger.h $(GTEST_HEADERS)
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -c $< -o $@
+
+$(TESTS): $(OBJS) gtest_main.a
+	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -lpthread $^ -o $@
---
+++ src/biginteger.h
@@ -0,0 +1,88 @@
+#ifndef BIGINTEGER_H
+#define BIGINTEGER_H
+/* 
+** file: biginteger.h
+*/
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+
+#define MAXINPUT 512 /*maximum input size(bytes) of number*/
+#define PLUS		1
+#define MINUS		-1
+/**
+ *  bigint structure
+ *  digits: array of int, contains input number
+ *  signbit: indicates negative or positive sign of the number
+ *  lastdigit: indicates number size
+ */
+typedef struct
+{
+    uint32_t digits[MAXINPUT];
+    int signbit;
+    int len;
+}bigint;
+
+/*x = x1 * B^m + x0;*/
+typedef struct
+{
+    uint32_t digits[MAXINPUT];
+    uint32_t x1[MAXINPUT/4];
+    uint32_t x0[MAXINPUT/4];
+    int len;
+    int base;
+    int m;
+}factored_num;
+
+int base_calc(bigint *in);
+void tenpow(bigint *n, int d);
+
+/**
+ * init_bigint function, initialize bigint with user input
+ * input: string with provided big number
+ * input_length: input length of provided input
+ * signbit: indicates negative or positive sign of the number(1 or -1)
+ * out: output bigint, should be provided from caller
+ */
+void init_bigint(const char *input, int input_length, bigint *out);
+
+/**
+ * add_bigint function, add two big numbers
+ * in0: input bigint
+ * in1: input bigint, will contains output
+ */
+void add_bigint(bigint *in0, bigint *in1);
+
+/**
+ * mult_bigint function, multiply two big numbers
+ * in0: input bigint
+ * in1: input bigint
+ * out: output bigint, should be provided by caller
+ */
+void mult_bigint(bigint *in0, bigint *in1, bigint *out);
+
+/**
+ * print_bigint, print bigint's digits field
+ * input: input bigint
+ */
+void print_bigint(const bigint *input);
+
+/**
+ * subtract_bigint function, subtracts two big numbers
+ * in0: input bigint
+ * in1: input bigint
+ * out: output bigint, should be provided by caller
+ */
+void subtract_bigint(bigint *in0, bigint *in1, bigint *out);
+
+/**
+ * bii_division function, multiply big number and scalar b
+ * in0: input bigint
+ * b: 2^64-1 number
+ * base: base of b 
+ * out: output put in in0
+ */
+void bii_division(bigint *n, long long b, long long base);
+#endif // BIGINTEGER_H
